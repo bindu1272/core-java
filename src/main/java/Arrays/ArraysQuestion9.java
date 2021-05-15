700156ae6fc91c9eefb7166a2a79dabbf6614e77
binduchowdary856 <binduchowdary856@gmail.com>
2021-05-15 21:23:45 +0530

    Basics,Static,Loops,Arrays Added

--- src/main/java/Arrays/ArraysQuestion9.java
+++ src/main/java/Arrays/ArraysQuestion9.java
@@ -1,4 +1,29 @@
 package main.java.Arrays;
+import java.util.Scanner;
 
 public class ArraysQuestion9 {
+    public static void main(String args[]) {
+        int sum = 0;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter Size of an array");
+        int num = sc.nextInt();
+        System.out.println("enter elements");
+        int array[] = new int[num];
+        for (int i = 0; i < num; i++) {
+            array[i] = sc.nextInt();
+        }
+        System.out.println("elements are :");
+        for (int i : array) {
+            System.out.print(i + " ");
+        }
+        for (int i = 0; i < num/2; i++) {
+            int temp = array[i];
+            array[i] = array[num-i-1];
+            array[num-i-1] = temp;
+        }
+        System.out.println("Reversed array: ");
+        for (int i : array) {
+            System.out.print(i + " ");
+        }
+    }
 }
